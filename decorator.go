b30af3e4d17f65aff0ba0ebcc0240fe53787d1d8
wuqi <wuqi198772@gmail.com>
2015-12-22 14:18:48 +0800

    Update decorator.go
    change the match color

--- decorator.go
+++ decorator.go
@@ -10,7 +10,7 @@ const (
 	ColorReset      = "\x1b[0m\x1b[K"
 	ColorLineNumber = "\x1b[1;33m"  /* yellow with black background */
 	ColorPath       = "\x1b[1;32m"  /* bold green */
-	ColorMatch      = "\x1b[30;43m" /* black with yellow background */
+	ColorMatch      = "\x1b[30;43m" /* blue with white background */
 
 	SeparatorColon  = ":"
 	SeparatorHyphen = "-"
